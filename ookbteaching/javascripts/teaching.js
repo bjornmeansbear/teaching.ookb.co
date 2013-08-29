1a9a8c5c7c91ba64c1b542e27efe34c480a9320e
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-29 16:47:18 -0400

    making links smooth scroll nicely...

--- ookbteaching/javascripts/teaching.js
+++ ookbteaching/javascripts/teaching.js
@@ -1,2 +1,8 @@
 
+// smooth scrolling links
+// scroll to content when you click on the logo. - retrieved from http://www.sycha.com/jquery-smooth-scrolling-internal-anchor-links
+$(".scroll").click(function(event){		
+	event.preventDefault();
+	$('html,body').animate({scrollTop:$(this.hash).offset().top-52}, 800);
+});
 
