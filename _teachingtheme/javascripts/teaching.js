af23cbb390278197e0044d25935f81fe011c8cce
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-09-23 22:24:37 -0400

    further calendar improvements

--- _teachingtheme/javascripts/teaching.js
+++ _teachingtheme/javascripts/teaching.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
   $('.calendar .week.past h2').click(function(event){
     event.preventDefault();
     //$('html,body').animate({scrollTop:$(this.hash).offset().top-52}, 800);
-    $('.calendar .week.past .row').toggle('slow');
+    $(this).parent().child('.row').toggle('slow');
   });
 
 });
